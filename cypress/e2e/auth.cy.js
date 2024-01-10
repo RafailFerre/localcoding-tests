39b24931f2ae18b35ab5aa9c598f99e864b7b56c
Rafail Ferre <qa.rafailferre@gmail.com>
2024-01-09 22:12:20 -0500

    Add 'it' test 'Sign in with invalid credential', add test scenarios

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -12,6 +12,18 @@ describe('Authorization', () => {
 
         cy.url().should('eq', 'https://coding.pasv.us/profile/62ec1c48a86ef3cf6d2be322')
         cy.get('.ant-avatar-square').should('be.visible')
+    })
 
+    it('Sign in with invalid credentials', () => {
+        //cy.visit('https://coding.pasv.us/user/login')
+        cy.get('#normal_login_email').type('ferrerafail@gmail.com') //valid email
+        cy.get('#normal_login_password').type('12345') //invalid password
+        cy.get('.login-form-button').should('be.visible')
+        cy.get('.login-form-button').click()
+
+        cy.url().should('eq', 'https://coding.pasv.us/user/login')
+        cy.get('.ant-notification-notice-message')
+            .should('be.visible')
+            .should('have.text', 'User login. Fail')
     })
 })
