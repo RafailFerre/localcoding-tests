d741d6c3197aa1db098eb96f8a49846b892cf026
Rafail Ferre <qa.rafailferre@gmail.com>
2024-01-11 04:06:44 -0500

    Add file cypress.env.json with env-variables, add in .gitignore. Add Cypress.env in 'it' test, add .idea, node_modules in .gitignore

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -5,8 +5,8 @@ describe('Authorization', () => {
 
     it('Sign in with valid credentials', () => {
         //cy.visit('https://coding.pasv.us/user/login')
-        cy.get('#normal_login_email').type('ferrerafail@gmail.com')
-        cy.get('#normal_login_password').type('Raf261085')
+        cy.get('#normal_login_email').type(Cypress.env('email'))
+        cy.get('#normal_login_password').type(Cypress.env('password'))
         cy.get('.login-form-button').should('be.enabled')
         cy.get('.login-form-button').click()
 
@@ -16,7 +16,7 @@ describe('Authorization', () => {
 
     it('Sign in with invalid credentials', () => {
         //cy.visit('https://coding.pasv.us/user/login')
-        cy.get('#normal_login_email').type('ferrerafail@gmail.com') //valid email
+        cy.get('#normal_login_email').type(Cypress.env('email')) //valid email
         cy.get('#normal_login_password').type('12345') //invalid password
         cy.get('.login-form-button').should('be.visible')
         cy.get('.login-form-button').click()
