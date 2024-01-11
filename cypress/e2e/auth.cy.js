aeb7c42bde1334455f12f2e285ab8a1defd8be34
Rafail Ferre <qa.rafailferre@gmail.com>
2024-01-10 20:24:05 -0500

    Add 'it' test 'Sign in form validation', add test scenarios

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -26,4 +26,55 @@ describe('Authorization', () => {
             .should('be.visible')
             .should('have.text', 'User login. Fail')
     })
+
+    it('Sign in form validation', () => {
+        //cy.get('https://coding.pasv.us/user/login')
+        cy.get('#normal_login_email').should('have.value', '')
+        cy.get('#normal_login_password').should('have.value', '')
+        cy.get('.login-form-button').should('be.disabled')
+
+        cy.get('#normal_login_email').type('test')
+        cy.get('#normal_login_email_help > div')
+            .should('be.visible')
+            .should('have.text', "'email' is not a valid email")
+        cy.get('.login-form-button').should('be.disabled')
+
+        cy.get('#normal_login_email').type('@')
+        cy.get('#normal_login_email_help > div')
+            .should('be.visible')
+            .should('have.text', "'email' is not a valid email")
+        cy.get('.login-form-button').should('be.disabled')
+
+        cy.get('#normal_login_email').type('email')
+        cy.get('#normal_login_email_help > div')
+            .should('be.visible')
+            .should('have.text', "'email' is not a valid email")
+        cy.get('.login-form-button').should('be.disabled')
+
+        cy.get('#normal_login_email').type('.')
+        cy.get('#normal_login_email_help > div')
+            .should('be.visible')
+            .should('have.text', "'email' is not a valid email")
+        cy.get('.login-form-button').should('be.disabled')
+
+        cy.get('#normal_login_email').type('com')
+        cy.get('#normal_login_email_help > div').should('not.exist')
+        cy.get('.login-form-button').should('be.disabled')
+
+        cy.get('#normal_login_password').type('test123')
+        cy.get('#normal_login_password_help > div').should('not.exist')
+        cy.get('.login-form-button').should('be.enabled')
+
+        cy.get('#normal_login_email').clear()
+        cy.get('#normal_login_email_help')
+            .should('be.visible')
+            .should('have.text', 'Required')
+        cy.get('.login-form-button').should('be.disabled')
+
+        cy.get('#normal_login_password').clear()
+        cy.get('#normal_login_password_help')
+            .should('be.visible')
+            .should('have.text', 'Required')
+        cy.get('.login-form-button').should('be.disabled')
+    })
 })
