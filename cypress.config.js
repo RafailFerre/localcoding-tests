03499a3f0224e284206d8135e66e0982767dfb47
Rafail Ferre <qa.rafailferre@gmail.com>
2024-01-23 02:53:20 -0500

    install prettier, add .prettierrc.json, .prettierignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-      baseUrl: 'https://coding.pasv.us',
-      excludeSpecPattern: [
-        'cypress/e2e/1-getting-started/*',
-        'cypress/e2e/2-advanced-examples/*',
-     ],
-      watchForFileChanges: false,
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
-  },
-});
+    e2e: {
+        baseUrl: 'https://coding.pasv.us',
+        excludeSpecPattern: [
+            'cypress/e2e/1-getting-started/*',
+            'cypress/e2e/2-advanced-examples/*',
+        ],
+        watchForFileChanges: false,
+        // setupNodeEvents(on, config) {
+        //   // implement node event listeners here
+        // },
+    },
+})
